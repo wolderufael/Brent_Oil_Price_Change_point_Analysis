6b5669838168472a669c4bead8e3f2f225db1de8
wolderufael <wolderuphaelkassahun@gmail.com>
2024-11-06 10:28:57 +0300

    build folder dist removed from .gitignore

--- DashBoard/React_Front_End/src/Component/Home.jsx
+++ DashBoard/React_Front_End/src/Component/Home.jsx
@@ -5,8 +5,11 @@ const Home = () => {
     <div className="search-params">
       <h3>
         This is a Dash board of Brent oil Price Time Series Analysis,Change
-        point Analysis and Prediction using LSTM and ARIMA Models.
-        For more information on the project: <a href="">Git Hub</a>
+        point Analysis and Prediction using LSTM and ARIMA Models. For more
+        information on the project:{" "}
+        <a href="https://github.com/wolderufael/Brent_Oil_Price_Change_point_Analysis" target="blank">
+          Git Hub
+        </a>
       </h3>
     </div>
   );
